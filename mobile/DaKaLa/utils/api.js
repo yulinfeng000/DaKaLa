d4b6a3b62dbbb5d56b269116b108de55bf0323e1
yulinfeng <yulinfeng.mail@foxmail.com>
2020-05-29 13:59:31 +0800

    移除mobile项目，将会单独开一个仓库

--- mobile/DaKaLa/utils/api.js
+++
@@ -1,6 +0,0 @@
-const BASE_URL = 'http://129.28.124.34:8888/api';
-
-export const REGISTER_URL = `${BASE_URL}/register`;
-export const PHOTO_URL = `${BASE_URL}/photo`;
-export const DAKA_URL = `${BASE_URL}/daka/nophoto`;
-export const DELETE_URL = `${BASE_URL}/delete`;
