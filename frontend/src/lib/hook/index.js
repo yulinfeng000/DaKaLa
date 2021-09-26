2be0af95dd0f08b2ea97d832805e5befc82c4ec9
Cam <791774086@qq.com>
2021-09-26 10:48:10 +0800

    update useSWR refresh interval

--- frontend/src/lib/hook/index.js
+++ frontend/src/lib/hook/index.js
@@ -7,10 +7,14 @@ import { getItem } from "../storage"
 
 export const useCurrentStuPhoto = () => {
   const student = getItem("student")
-  const { data, error } = useSWR(`/stu/${student.stuid}/photo`, async (url) => {
-    const resp = await Axios.get(url, { responseType: "arraybuffer" })
-    return Buffer.from(resp.data, "base64").toString("base64")
-  })
+  const { data, error } = useSWR(
+    `/stu/${student.stuid}/photo`,
+    async (url) => {
+      const resp = await Axios.get(url, { responseType: "arraybuffer" })
+      return Buffer.from(resp.data, "base64").toString("base64")
+    },
+    { refreshInterval: 2000 }
+  )
 
   return {
     photo: data ? data : "",
